823322991db07b1c24d539df0e74f77eb29f26cb
UniGiu <g.tilli@studenti.unibg.it>
2025-01-19 22:51:21 +0100

    prima stesura pagina invitations

--- codice/washine-vaadin/src/main/java/uni/washine/application/views/home/HomeView.java
+++ codice/washine-vaadin/src/main/java/uni/washine/application/views/home/HomeView.java
@@ -12,10 +12,10 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
 import com.vaadin.flow.server.VaadinSession;
 
-
 import org.vaadin.lineawesome.LineAwesomeIconUrl;
 
 import uni.washine.application.utils.AuthenticationComponent;
+
 @PageTitle("Home")
 @Route("")
 @Menu(order = 0, icon = LineAwesomeIconUrl.HOME_SOLID)
@@ -25,7 +25,7 @@ public class HomeView extends Composite<VerticalLayout> {
         HorizontalLayout layoutRow = new HorizontalLayout();
         H1 h1 = new H1();
         Paragraph textMedium = new Paragraph();
-        
+
         getContent().setWidth("100%");
         getContent().getStyle().set("flex-grow", "1");
         layoutRow.addClassName(Gap.MEDIUM);
@@ -37,138 +37,18 @@ public class HomeView extends Composite<VerticalLayout> {
                 "Whashine is a Washing Machine sharing utility that facilitates you in sharing your washing machine with people you trust.");
         textMedium.setWidth("100%");
         textMedium.getStyle().set("font-size", "var(--lumo-font-size-m)");
-        
+
         getContent().add(layoutRow);
         layoutRow.add(h1);
         getContent().add(textMedium);
-        if(VaadinSession.getCurrent().getAttribute("currentUser")==null){
-           // layoutColumn2.add(getLoginForm());
-          //  layoutColumn2.add(getSignInForm());
-            AuthenticationComponent authComp =new AuthenticationComponent();
+        if (VaadinSession.getCurrent().getAttribute("currentUser") == null) {
+            // layoutColumn2.add(getLoginForm());
+            // layoutColumn2.add(getSignInForm());
+            AuthenticationComponent authComp = new AuthenticationComponent();
             authComp.setWidth("100%");
             authComp.getStyle().set("flex-grow", "1");
-            
+
             getContent().add(authComp);
-           }
-    }
- /*   private VerticalLayout getLoginForm(){
-        VerticalLayout layoutColumnLogin = new VerticalLayout();
-        H2 h2Login = new H2();
-        EmailField emailFieldLogin = new EmailField();
-        PasswordField passwordFieldLogin = new PasswordField();
-        Button buttonLogin = new Button();
-
-        layoutColumnLogin.setWidth("100%");
-        layoutColumnLogin.getStyle().set("flex-grow", "1");
-        h2Login.setText("Log in");
-        h2Login.setWidth("max-content");
-        emailFieldLogin.setLabel("Email *");
-        emailFieldLogin.setWidth("min-content");
-        passwordFieldLogin.setLabel("Password *");
-        passwordFieldLogin.setWidth("min-content");
-        buttonLogin.setText("Log in");
-        buttonLogin.setWidth("min-content");
-        buttonLogin.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-        layoutColumnLogin.add(h2Login);
-        layoutColumnLogin.add(emailFieldLogin);
-        layoutColumnLogin.add(passwordFieldLogin);
-        layoutColumnLogin.add(buttonLogin);
-
-        buttonLogin.addClickListener(e -> {
-            String email = emailFieldLogin.getValue();
-            String password = passwordFieldLogin.getValue();
-            
-            if (validateAuthInput(email, password)) {
-                try {
-          handleLogin(email, password);
-        } catch (SQLException e1) {
-          // TODO Auto-generated catch block
-        e1.printStackTrace();
-        }
-            }
-        });
-
-        return layoutColumnLogin;
-    }
-    private VerticalLayout getSignInForm(){
-        VerticalLayout layoutColumnSignIn = new VerticalLayout();
-        H2 h2SignIn = new H2();
-        EmailField emailFieldSignIn = new EmailField();
-        PasswordField passwordFieldSignIn = new PasswordField();
-        Button buttonSignIn = new Button();
-
-        layoutColumnSignIn.setWidth("100%");
-        layoutColumnSignIn.getStyle().set("flex-grow", "1");
-        h2SignIn.setText("Subscribe");
-        h2SignIn.setWidth("max-content");
-        emailFieldSignIn.setLabel("Email *");
-        emailFieldSignIn.setWidth("min-content");
-        passwordFieldSignIn.setLabel("Password *");
-        passwordFieldSignIn.setWidth("min-content");
-        buttonSignIn.setText("Subscribe");
-        buttonSignIn.setWidth("min-content");
-        buttonSignIn.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-        layoutColumnSignIn.add(h2SignIn);
-        layoutColumnSignIn.add(emailFieldSignIn);
-        layoutColumnSignIn.add(passwordFieldSignIn);
-        layoutColumnSignIn.add(buttonSignIn);
-
-        buttonSignIn.addClickListener(e -> {
-            String email = emailFieldSignIn.getValue();
-            String password = passwordFieldSignIn.getValue();
-            
-            if (validateAuthInput(email, password)) {
-               try {
-        handleSignUp(email, password);
-      } catch (SQLException e1) {
-        // TODO Auto-generated catch block
-        e1.printStackTrace();
-      }
-            }
-        });
-
-        return layoutColumnSignIn;
-    }
-   
-    private boolean validateAuthInput(String email, String password) {
-        if (email.isEmpty() || password.isEmpty()) {
-        	UiNotifier.showErrorNotification("Please fill in all required fields");
-            return false;
-        }
-        if (password.length() < 8) {
-        	UiNotifier.showErrorNotification("Password must be at least 8 characters long");
-            return false;
-        }
-      
-        if (!email.matches("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")) {
-        	UiNotifier.showErrorNotification("Please enter a valid email address");
-            return false;
-        }
-        return true;
-    }
-
-    private void handleLogin(String email, String password) throws SQLException {
-        WashineCoreAuthIf wCore=new WashineCoreAuth();
-        WashineUserIf loggedUser = wCore.authenticateUser(email, password);
-
-        if (loggedUser!=null) {
-            VaadinSession.getCurrent().setAttribute("currentUser", loggedUser);
-            getUI().ifPresent(ui -> ui.getPage().reload());
-        } else {
-        	UiNotifier.showErrorNotification("Invalid email or password");
         }
     }
-
-    private void handleSignUp(String email, String password) throws SQLException {
-    	  WashineCoreAuthIf wCore=new WashineCoreAuth();
-          WashineUserIf createdUser = wCore.addUser(email, password);        
-        if (createdUser!=null) {          
-        	UiNotifier.showSuccessNotification("Account created for " + createdUser.getEmail()+ ", please log in.");           
-        } else {
-        	UiNotifier.showErrorNotification("Invalid email or password, or user already registered");
-        }
-    }
-   */
 }
--- codice/washine-vaadin/src/main/java/uni/washine/application/views/joinalaundrycomunity/JoinALaundryComunityView.java
+++ codice/washine-vaadin/src/main/java/uni/washine/application/views/joinalaundrycomunity/JoinALaundryComunityView.java
@@ -5,7 +5,10 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Hr;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -18,95 +21,185 @@ import com.vaadin.flow.server.VaadinSession;
 import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
 
 import uni.washine.application.utils.AuthenticationComponent;
+import washine.washineCore.WashineCoreCommunity;
+import washine.washineCore.WashineCoreCommunityIf;
 import washine.washineCore.user.WashineUserIf;
-
+import uni.washine.application.utils.UiNotifier;
 import org.vaadin.lineawesome.LineAwesomeIconUrl;
 
 @PageTitle("Invitations")
 @Route("invitations")
 @Menu(order = 5, icon = LineAwesomeIconUrl.SIGN_IN_ALT_SOLID)
-public class JoinALaundryComunityView extends Composite<VerticalLayout>  implements BeforeEnterObserver{
+public class JoinALaundryComunityView extends Composite<VerticalLayout> implements BeforeEnterObserver {
 
 	private WashineUserIf userData;
 
-    public JoinALaundryComunityView() {
-        HorizontalLayout layoutRow = new HorizontalLayout();
-        H2 h2 = new H2();
-        Paragraph textLarge = new Paragraph();
-        VerticalLayout layoutColumn2 = new VerticalLayout();
-        FormLayout formLayout2Col = new FormLayout();
-        TextField textField = new TextField();
-        TextField textField2 = new TextField();
-        Button buttonPrimary = new Button();
-        getContent().setWidth("100%");
-        getContent().getStyle().set("flex-grow", "1");
-        layoutRow.addClassName(Gap.MEDIUM);
-        layoutRow.setWidth("100%");
-        layoutRow.setHeight("min-content");
-        h2.setText("Join a Loundry Community");
-        h2.setWidth("max-content");
-        textLarge.setText(
-                "Enter the code you received by a machine owner to join their laundry community. Choose a meaningful community name so that you will be able to understand who is offering the wash");
-        textLarge.setWidth("100%");
-        textLarge.getStyle().set("font-size", "var(--lumo-font-size-xl)");
-        layoutColumn2.setWidth("100%");
-        layoutColumn2.getStyle().set("flex-grow", "1");
-        formLayout2Col.setWidth("100%");
-        textField.setLabel("Join code");
-        textField.setWidth("min-content");
-        textField2.setLabel("Community name");
-        textField2.setWidth("min-content");
-        buttonPrimary.setText("Join");
-        buttonPrimary.setWidth("min-content");
-        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        getContent().add(layoutRow);
-        layoutRow.add(h2);
-        getContent().add(textLarge);
-        getContent().add(layoutColumn2);
-        layoutColumn2.add(formLayout2Col);
-        formLayout2Col.add(textField);
-        formLayout2Col.add(textField2);
-        formLayout2Col.add(buttonPrimary);
+	public JoinALaundryComunityView() {
+		HorizontalLayout layoutRow = new HorizontalLayout();
+		H2 h2 = new H2();
+		Paragraph textLarge = new Paragraph();
 
-        
-    }
-    
-/**
- * Shows authentication component to anonymous users
- */
-  @Override
-  public void beforeEnter(BeforeEnterEvent event) {
-	  userData = (WashineUserIf) VaadinSession.getCurrent().getAttribute("currentUser");
-	  if(userData==null) {
-        event.rerouteTo("anonymous-user");
-		  //event.rerouteTo(AnonymousUser.class);
-	  }
-  }
+		VerticalLayout layoutColumn2 = new VerticalLayout();
+		FormLayout layoutCodeIn = getCodeInsertionLayout();
+		Hr hr = new Hr();
+		FormLayout layoutCodeGen = getCodeGenerationLayout();
+
+		getContent().setWidth("100%");
+		getContent().getStyle().set("flex-grow", "1");
+		layoutRow.addClassName(Gap.MEDIUM);
+		layoutRow.setWidth("100%");
+		layoutRow.setHeight("min-content");
+		h2.setText("Invitations");
+		h2.setWidth("max-content");
+		textLarge.setText("Here you can join to a launder washing community or invite people to your own laundry");
+		textLarge.setWidth("100%");
+		textLarge.getStyle().set("font-size", "var(--lumo-font-size-xl)");
+		layoutColumn2.setWidth("100%");
+		layoutColumn2.getStyle().set("flex-grow", "1");
+
+		getContent().add(layoutRow);
+		layoutRow.add(h2);
+		getContent().add(textLarge);
+		getContent().add(layoutColumn2);
+
+		layoutColumn2.add(layoutCodeIn, hr, layoutCodeGen);
+	}
+
+	private FormLayout getCodeInsertionLayout() {
+		FormLayout formLayout2Col = new FormLayout();
+		H3 h3Join = new H3();
+		Paragraph textJoin = new Paragraph();
+		TextField textFieldCode = new TextField();
+		TextField textFieldCommunity = new TextField();
+		Button buttonJoin = new Button();
+		h3Join.setText("Join a community");
+		textJoin.setText(
+				"Enter the code you received by a machine owner to join their laundry community. Choose a meaningful community name so that you will be able to understand who is offering the wash");
+		formLayout2Col.setWidth("100%");
+		textFieldCode.setLabel("Join code");
+		textFieldCode.setWidth("min-content");
+		textFieldCommunity.setLabel("Community name");
+		textFieldCommunity.setWidth("min-content");
+		buttonJoin.setText("Join");
+		buttonJoin.setWidth("min-content");
+		buttonJoin.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+		formLayout2Col.add(textFieldCode);
+		formLayout2Col.add(textFieldCommunity);
+		formLayout2Col.add(buttonJoin);
+
+		buttonJoin.addClickListener(event -> {
+			String code = textFieldCode.getValue();
+			String communityName = textFieldCommunity.getValue();
+			String uid = userData.getId();
+			
+			WashineCoreCommunityIf community = new WashineCoreCommunity();
+            String communityId=community.getInvitationCodeCommunityId(code);
+            if(code==null){
+                UiNotifier.showErrorNotification("Wrong or expired code.");
+            }else{
+                if (community.userInCommunity(uid, communityName)) {
+                    UiNotifier.showErrorNotification("You already joined this community.");                    
+                } else {
+                    if(community.nameInJoinedCommunities(uid, communityName)){
+                        UiNotifier.showErrorNotification("You already used this name for another community you joined.");                    
+                    }else{
+                        //User can finally join the community!
+                        if(community.joinCommunity(uid, communityName, code)){
+                            UiNotifier.showSuccessNotification("Congratulations! Join successful!");
+                        }
+                    }
+                }
+
+			}
+		});
+		
+		
+		
+		return formLayout2Col;
+	}
+
+	private FormLayout getCodeGenerationLayout() {
+		FormLayout formLayout2Col = new FormLayout();
+		H3 h3Generate = new H3();
+		
+		TextField textFieldParticipant = new TextField();
+		Button buttonGenerate = new Button();
+		Paragraph invitationLinkText = new Paragraph();
+		Paragraph invitationCodeText = new Paragraph();
+		h3Generate.setText(
+				"Press the generate button and share the code with the invitee. Choose a meaningful namename of the person you want to add to your washing community,");
+		formLayout2Col.setWidth("100%");
+		textFieldParticipant.setLabel("Participant name");
+		textFieldParticipant.setWidth("min-content");
+		buttonGenerate.setText("Generate");
+		buttonGenerate.setWidth("min-content");
+		buttonGenerate.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+		formLayout2Col.add(textFieldParticipant);
+		formLayout2Col.add(buttonGenerate);
+		formLayout2Col.add(invitationLinkText);
+
+		buttonGenerate.addClickListener(event -> {
+			String newUserName = textFieldParticipant.getValue();
+			String uid = userData.getId();
+			
+			WashineCoreCommunityIf community = new WashineCoreCommunity();
+
+			if (community.nameInCommunity(uid, newUserName)) {
+				UiNotifier.showErrorNotification("This name is already taken.");
+				invitationLinkText.setText("");
+				invitationCodeText.setText("");
+			} else {
+				String invitationCode = community.getNewInvitationCode(uid, newUserName);
+				if(invitationCode!=null) {
+					String invitationLink = "https://www.washine.uni/invitation?icode=" + invitationCode;
+					invitationCodeText.setText("");
+					invitationLinkText.setText("Share this link: " + invitationLink); // Update the paragraph with the link
+				}else {
+					UiNotifier.showErrorNotification("The code could not be created.");
+				}
+				
+			}
+		});
+
+		return formLayout2Col;
+	}
+
+	/**
+	 * Shows authentication component to anonymous users
+	 */
+	@Override
+	public void beforeEnter(BeforeEnterEvent event) {
+		userData = (WashineUserIf) VaadinSession.getCurrent().getAttribute("currentUser");
+		if (userData == null) {
+			event.rerouteTo("anonymous-user");
+			// event.rerouteTo(AnonymousUser.class);
+		}
+	}
 }
+
 @PageTitle("Invitations")
 @Route("anonymous-user")
-    class AnonymousUser extends Composite<VerticalLayout> {
-        public AnonymousUser(){
-            HorizontalLayout layoutRow = new HorizontalLayout();
-            H2 h2 = new H2();
-            Paragraph textLarge = new Paragraph();
-            getContent().setWidth("100%");
-            getContent().getStyle().set("flex-grow", "1");
-            layoutRow.addClassName(Gap.MEDIUM);
-            layoutRow.setWidth("100%");
-            layoutRow.setHeight("min-content");
-            h2.setText("Join a Loundry Community");
-            h2.setWidth("max-content");
-            textLarge.setText(
-                    "To manage invitations you must login");
-            textLarge.setWidth("100%");
-            textLarge.getStyle().set("font-size", "var(--lumo-font-size-xl)");
-            getContent().add(layoutRow);
-            layoutRow.add(h2);
-            getContent().add(textLarge);
-            
-            AuthenticationComponent authComp =new AuthenticationComponent();
-            getContent().add(authComp);
-        }
-        
-    }
+class AnonymousUser extends Composite<VerticalLayout> {
+	public AnonymousUser() {
+		HorizontalLayout layoutRow = new HorizontalLayout();
+		H2 h2 = new H2();
+		Paragraph textLarge = new Paragraph();
+		getContent().setWidth("100%");
+		getContent().getStyle().set("flex-grow", "1");
+		layoutRow.addClassName(Gap.MEDIUM);
+		layoutRow.setWidth("100%");
+		layoutRow.setHeight("min-content");
+		h2.setText("Join a Loundry Community");
+		h2.setWidth("max-content");
+		textLarge.setText("To manage invitations you must login");
+		textLarge.setWidth("100%");
+		textLarge.getStyle().set("font-size", "var(--lumo-font-size-xl)");
+		getContent().add(layoutRow);
+		layoutRow.add(h2);
+		getContent().add(textLarge);
+
+		AuthenticationComponent authComp = new AuthenticationComponent();
+		getContent().add(authComp);
+	}
+
+}
