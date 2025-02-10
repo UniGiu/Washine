2e58b554ff037361dd3bb02edc0a627af660a6b5
UniGiu <g.tilli@studenti.unibg.it>
2025-02-10 01:32:46 +0100

    rimossi import inutilizzzati

--- codice/washine-vaadin/src/main/java/uni/washine/application/views/mymachine/LaunderWashingsList.java
+++ codice/washine-vaadin/src/main/java/uni/washine/application/views/mymachine/LaunderWashingsList.java
@@ -1,15 +1,6 @@
 package uni.washine.application.views.mymachine;
-
-import java.util.List;
-
-import com.vaadin.flow.server.VaadinSession;
-
-import uni.washine.application.utils.UiNotifier;
 import uni.washine.application.utils.WashingsList;
-import washine.washineCore.exceptions.WashineCoreException;
-import washine.washineCore.user.WashineUserIf;
 import washine.washineCore.washing.WashineLaundryWashingIf;
-
 
 public class LaunderWashingsList extends WashingsList{
 	
