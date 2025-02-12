a350a803eeb244341da9bde352d7619ce294ca04
Angelo Gargantini <angelo.gargantini@unibg.it>
2025-02-12 14:09:26 +0100

    eleimnato generated

--- codice/washineCore/src/main/java/washine/washineCore/CoreFactoryDefault.java
+++ codice/washineCore/src/main/java/washine/washineCore/CoreFactoryDefault.java
@@ -1,19 +1,19 @@
 package washine.washineCore;
 
 public class CoreFactoryDefault implements AbstractCoreFactory {
-	
-		private static CoreFactoryDefault instance;
-		
-	
-		private CoreFactoryDefault() {}
-		
-		
-		public static AbstractCoreFactory getInstance() {
-			if (instance == null) {			
-				instance = new CoreFactoryDefault();			
-			}
-			return instance;
+
+	private static CoreFactoryDefault instance;
+
+	private CoreFactoryDefault() {
+	}
+
+	public static AbstractCoreFactory getInstance() {
+		if (instance == null) {
+			instance = new CoreFactoryDefault();
 		}
+		return instance;
+	}
+
 	@Override
 	public WashineCoreAuthIf createCoreAuth() {
 		// TODO Auto-generated method stub
